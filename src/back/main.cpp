99c1a65c536c2ec87097209e116389970ca77e8a
EEdle <83777873+EEdle506@users.noreply.github.com>
2022-09-19 16:48:12 +0300

    Changed some settings

--- src/back/main.cpp
+++ src/back/main.cpp
@@ -13,16 +13,21 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationName("Calculator QML");
 #endif
 
-	QGuiApplication app(argc, argv);
-	app.setWindowIcon(QIcon("qrc:/app/assets/images/calculator.png"));
+    QGuiApplication app(argc, argv);
+    QGuiApplication::setWindowIcon(QIcon("qrc:/app/assets/images/calculator.png"));
 
-    QScopedPointer<History> history(new History());
+    // history is static object
+    QScopedPointer <History> history(new History());
     qmlRegisterSingletonInstance("Calculator.History", 1, 0, "History", history.get());
-    qmlRegisterType<Standart>("Calculator.Standart", 1, 0, "Standart");
 
-    QScopedPointer<Converter> converter(new Converter());
-    qmlRegisterSingletonInstance("Calculator.Converter", 1, 0, "Converter", converter.get());
+    qmlRegisterType <Standart>("Calculator.Standart", 1, 0, "Standart");
 
+    // converter handler is static object
+	QScopedPointer <ConverterHandler> cnvtHandler(new ConverterHandler());
+    qmlRegisterSingletonInstance("Calculator.ConverterHandler", 1, 0, "ConverterHandler", cnvtHandler.get());
+
+    qmlRegisterType <Converter>("Calculator.Converter", 1, 0, "Converter");
+    
     QQmlApplicationEngine engine;
 	engine.load(QUrl("qrc:/app/qml/main.qml"));
 	if (engine.rootObjects().isEmpty())
